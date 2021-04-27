2463e27451fe60bd1319ca5fee26e135483941d4
Shahab Uddin Ahmed <shahabuddin2401026@gmail.com>
2021-04-27 13:09:46 -0700

    1st day

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.write(5 + true + "sylani");
+// document.write("sylani5" + 5 + 5);
+// document.write(true + true + 5 * 9 / 8 + "sylani");
+// var name="shahab uddin";
+// var name1="5";
+
+// document.write(name1 ** 3);
