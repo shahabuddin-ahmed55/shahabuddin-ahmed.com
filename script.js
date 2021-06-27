6037737875dab1e95e47ac12029f2c01e814b624
Shahab Uddin Ahmed <shahabuddin2401026@gmail.com>
2021-06-27 20:01:01 +0500

    Practice

--- script.js
+++ script.js
@@ -5,3 +5,37 @@
 // var name1="5";
 
 // document.write(name1 ** 3);
+
+// var num = prompt("Enter number");
+
+// if (num % 3 === 0 && num % 5 ===0) {
+//     console.log("FizzBuzz");
+// } else if (num % 3 === 0) {
+//     console.log("Fizz");
+// } else if (num % 5 ===0) {
+//     console.log("Buzz");
+// } else {
+//     console.log(num);
+// }
+
+// var num = +prompt("Enter Number");
+
+// if (!isNaN(num)) {
+//     if (num % 2 === 0) {
+//         document.write("This number is Even");
+//         console.log("This number is Even");
+//     } else {
+//         document.write("This number is odd");
+//         console.log("This no is odd");
+//     }
+// } else {
+//     document.write("This is not a  number");
+//     console.log("This is not a  number");
+// }
+
+var table = +prompt("which table you want");
+
+for (var i = 1; i < 11; i++) {
+    console.log(table + "x" + i + "=" + table*i);
+    document.write(table + "x" + i + "=" + table*i);
+}
